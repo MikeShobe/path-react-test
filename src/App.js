82f3955362595ada1aed818910d6e3737d3211de
Mike <mikeshobe@yahoo.com>
2018-10-18 03:36:30 -0600

    Material-ui, basic ui & functions
    Added Material-UI, searchbar and submit button set up with simple functions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
+  state = {
+    githubName: '',
+  }
+
+  handleChange = (e) => {
+    e.preventDefault();
+    this.setState({ githubName: e.target.value });
+  }
+
+  handleSearch = (e) => {
+    e.preventDefault();
+    console.log('SUBMITTED', this.state.githubName)
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Grid alignItems='center' container direction='column' justify='center' style={{ paddingTop: '50px' }}>
+        <Grid item>
+          <header>
+            <h1>GitHub Repo Search</h1>
+          </header>
+        </Grid>
+        <Grid item>
+          <form noValidate autoComplete='off'>
+            <Grid alignItems='center' container direction='row'>
+              <Grid item xs={9}>
+                <TextField
+                  id='github_name'
+                  label='GitHub User Name'
+                  value={this.state.githubName}
+                  onChange={this.handleChange}
+                  margin='normal'
+                />
+              </Grid>
+              <Grid item xs={3}>
+                <Button onClick={this.handleSearch} variant='outlined'>
+                  Search
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </Grid>
+      </Grid>
     );
   }
 }
