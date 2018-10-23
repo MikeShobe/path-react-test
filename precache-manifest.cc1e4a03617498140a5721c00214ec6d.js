b6da12ab4d018fc336d35a7261c519ae2dfa89cc
Mike <mikeshobe@yahoo.com>
2018-10-23 10:05:20 +0900

    Updates

---
+++ precache-manifest.cc1e4a03617498140a5721c00214ec6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c012d556288991c9056",
+    "url": "/path-react-test/static/js/runtime~main.9c012d55.js"
+  },
+  {
+    "revision": "6fab29e49fb469e54707",
+    "url": "/path-react-test/static/js/main.6fab29e4.chunk.js"
+  },
+  {
+    "revision": "0f0a66a4df6a59acc4c6",
+    "url": "/path-react-test/static/js/1.0f0a66a4.chunk.js"
+  },
+  {
+    "revision": "6fab29e49fb469e54707",
+    "url": "/path-react-test/static/css/main.4bbabc16.chunk.css"
+  },
+  {
+    "revision": "0c63b6451662c114ebdf61073509a5cb",
+    "url": "/path-react-test/index.html"
+  }
+];
